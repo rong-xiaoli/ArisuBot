1dfde900316a65c087c820a3865764d68f32e352
容小狸 <hashnoob079@163.com>
2025-01-12 02:24:56 +0800

    Added AutoAccept. Changed EmergencyStop subcommand name.

--- src/main/java/top/rongxiaoli/backend/EventListener.java
+++ src/main/java/top/rongxiaoli/backend/EventListener.java
@@ -2,7 +2,8 @@ package top.rongxiaoli.backend;
 
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
-import net.mamoe.mirai.event.events.NudgeEvent;
+import net.mamoe.mirai.event.events.*;
+import top.rongxiaoli.plugins.AutoAccept.AutoAccept;
 import top.rongxiaoli.plugins.PokeReact.PokeReact;
 
 /**
@@ -12,5 +13,21 @@ public class EventListener extends SimpleListenerHost {
     @EventHandler
     public void onPoke(NudgeEvent e) {
         PokeReact.INSTANCE.onNudgeEvent(e);
+    }
+    @EventHandler
+    public void onBotInvitedJoinGroupRequestEvent(BotInvitedJoinGroupRequestEvent e) {
+        AutoAccept.INSTANCE.onBotInvitedJoinGroupRequestEvent(e);
+    }
+    @EventHandler
+    public void onFriendAddEvent(FriendAddEvent e) {
+        AutoAccept.INSTANCE.onFriendAddEvent(e);
+    }
+    @EventHandler
+    public void onBotJoinGroupEvent(BotJoinGroupEvent e) {
+        AutoAccept.INSTANCE.onBotJoinGroupEvent(e);
+    }
+    @EventHandler
+    public void onNewFriendRequestEvent(NewFriendRequestEvent e) {
+        AutoAccept.INSTANCE.onNewFriendRequestEvent(e);
     }
 }
