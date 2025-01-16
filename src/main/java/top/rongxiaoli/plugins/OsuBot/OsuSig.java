be065d4814c07e842094dd120732eb740b45c71b
容小狸 <hashnoob079@163.com>
2025-01-17 03:45:06 +0800

    Finish osu!sig bot. osu!bot is not planned to finish in 1 version.

--- src/main/java/top/rongxiaoli/plugins/OsuBot/OsuSig.java
+++ src/main/java/top/rongxiaoli/plugins/OsuBot/OsuSig.java
@@ -18,6 +18,7 @@ import top.rongxiaoli.plugins.OsuBot.backend.osusig.dataStruct.OsuSigSettings;
 import top.rongxiaoli.plugins.OsuBot.data.OsuData;
 import top.rongxiaoli.plugins.OsuBot.utils.OsuUtils;
 
+import java.util.Locale;
 import java.util.Objects;
 
 @Plugin(name = "OsuBot")
@@ -33,8 +34,18 @@ public class OsuSig extends ArisuBotAbstractCompositeCommand {
     @Description("获取osu!sig的使用帮助")
     public void getHelp(CommandContext context) {
         MessageChainBuilder mcb = new MessageChainBuilder();
-        mcb.add("你可以使用osu!sig获取一张简要的osu!成绩图。\n");
-        mcb.add("你可以设置背景颜色");
+        mcb.add("你可以使用osu!sig获取一张简要的osu!成绩图：osusig get\n");
+        mcb.add("你可以设置背景颜色: osusig setBackground\n");
+        mcb.add("设置游戏模式：osusig setGameMode\n");
+        mcb.add("设置是否显示国家排名：osusig setCountryRank\n");
+        mcb.add("设置是否替换PP为Rank分osusig setReplacePPWithRankScore\n");
+        mcb.add("设置PP显示模式：osusig setPPShowMode\n");
+        mcb.add("设置是否显示经验条：osusig setExpVisible\n");
+        mcb.add("设置经验条是否与背景同色osusig setSameBackgroundColor\n");
+        mcb.add("子命令有简写，具体可以访问ArisuBot项目的Wiki来看。\n");
+        mcb.add("具体可以使用osusig help 加子命令名来获取详细帮助\n");
+        mcb.add("感谢由 神代绮凛 提供的API，他提供了很有帮助的功能\n");
+        mcb.add("项目地址：https://osusig.lolicon.app");
         context.getSender().sendMessage(mcb.build());
     }
     @SubCommand("help")
@@ -45,8 +56,9 @@ public class OsuSig extends ArisuBotAbstractCompositeCommand {
             default:
                 mcb.add("请选择正确的子命令。");
                 break;
-            case "sigsetbackground":
-            case "ssbg":
+            case "setbackground":
+            case "sbg":
+            case "背景":
                 mcb.add("设置名片背景颜色\n");
                 mcb.add("参数有：\n");
                 mcb.add(OsuSigEnum.BackgroundColor.BLACK + "表示黑色；\n");
@@ -59,23 +71,72 @@ public class OsuSig extends ArisuBotAbstractCompositeCommand {
                 mcb.add(OsuSigEnum.BackgroundColor.PURPLE_RED + "表示紫红色；\n");
                 mcb.add(OsuSigEnum.BackgroundColor.RED + "表示红色；\n");
                 mcb.add(OsuSigEnum.BackgroundColor.YELLOW + "表示黄色；\n");
-                mcb.add("请注意，区分大小写。");
                 break;
-            case "ssgm":
-            case "sigsetgamemode":
+            case "setgamemode":
+            case "sgm":
+            case "模式":
                 mcb.add("设置名片游戏模式\n");
                 mcb.add("参数有：\n");
                 mcb.add(OsuSigEnum.GameMode.CATCH + "表示接果；\n");
                 mcb.add(OsuSigEnum.GameMode.MANIA + "表示弹琴；\n");
                 mcb.add(OsuSigEnum.GameMode.STANDARD + "表示标准；\n");
                 mcb.add(OsuSigEnum.GameMode.TAIKO + "表示太鼓；\n");
-                //mcb.add("请注意，区分大小写。");
+                break;
+            case "setppshowmode":
+            case "sppsm":
+            case "pp":
+            case "pp显示模式":
+                mcb.add("设置PP显示模式\n");
+                mcb.add("参数有：\n");
+                mcb.add(OsuSigEnum.PPShowMode.NO_DISPLAY + "表示不显示\n");
+                mcb.add(OsuSigEnum.PPShowMode.REPLACE_LEVEL + "表示替换等级\n");
+                mcb.add(OsuSigEnum.PPShowMode.AFTER_ACC + "表示在Acc之后显示\n");
+                mcb.add(OsuSigEnum.PPShowMode.AFTER_RANK + "表示在Rank之后显示\n");
+                mcb.add("不区分大小写，加了一些关键词针对解析");
+                break;
+            case "setcountryrank":
+            case "scr":
+            case "国家排名":
+            case "排名":
+                mcb.add("设置是否显示国家排名");
+                mcb.add("这是一个布尔值，只有是和否；\n");
+                mcb.add("你也可以使用true, false或显示，不显示来设置");
+                break;
+            case "srppwrs":
+            case "setreplaceppwithrankscore":
+            case "替换pp为rank分":
+            case "替换":
+                mcb.add("设置是否替换PP为Rank分");
+                mcb.add("这是一个布尔值，只有是和否；\n");
+                mcb.add("你也可以使用true, false或显示，不显示来设置");
+                break;
+            case "setexpvisible":
+            case "sev":
+            case "exp":
+            case "经验":
+            case "经验条":
+            case "显示经验":
+                mcb.add("设置是否显示经验条");
+                mcb.add("这是一个布尔值，只有是和否；\n");
+                mcb.add("你也可以使用true, false或显示，不显示来设置");
+                break;
+            case "setsamebackgroundcolor":
+            case "ssbc":
+            case "经验条背景同色":
+            case "同色":
+            case "经验条同色":
+                mcb.add("设置是否统一背景和经验条颜色");
+                mcb.add("这是一个布尔值，只有是和否；\n");
+                mcb.add("你也可以使用true, false或显示，不显示来设置");
+                break;
+            case "get":
+                mcb.add("获取osu!sig成绩图");
                 break;
         }
         context.getSender().sendMessage(mcb.build());
     }
 
-    @SubCommand({"sigSetBackground", "ssbg", "背景"})
+    @SubCommand({"setBackground", "sbg", "背景"})
     @Description("设置名片背景颜色")
     public void setOsuSigBackgroundColor(CommandContext context, OsuSigEnum.BackgroundColor color) {
         if (!pluginStatus) return;
@@ -107,7 +168,7 @@ public class OsuSig extends ArisuBotAbstractCompositeCommand {
         context.getSender().sendMessage("已将颜色设置为：" + color);
     }
 
-    @SubCommand({"sigSetGameMode", "ssgm", "模式"})
+    @SubCommand({"setGameMode", "sgm", "模式"})
     @Description("设置游戏模式")
     public void setOsuSigGameMode(CommandContext context, String gameModeStr) {
         if (!pluginStatus) return;
@@ -135,23 +196,273 @@ public class OsuSig extends ArisuBotAbstractCompositeCommand {
         switch (gameModeStr.toLowerCase()) {
             default:
                 context.getSender().sendMessage("没有这个模式");
+                return;
             case "接果":
             case "catch":
                 settings.setGameMode(OsuSigEnum.GameMode.CATCH);
+                break;
             case "弹琴":
             case "mania":
                 settings.setGameMode(OsuSigEnum.GameMode.MANIA);
+                break;
             case "太鼓":
             case "taiko":
                 settings.setGameMode(OsuSigEnum.GameMode.TAIKO);
+                break;
             case "标准":
             case "standard":
             case "osu":
                 settings.setGameMode(OsuSigEnum.GameMode.STANDARD);
+                break;
         }
         OsuSigBaseData newData = new OsuSigBaseData(settings);
         OsuSigUtils.setBaseData(userID, newData);
         context.getSender().sendMessage("已设置: " + gameModeStr);
+    }
+    @SubCommand({"setCountryRank", "scr", "国家排名", "排名"})
+    @Description("设置是否显示国家排名")
+    public void setCountryRankVisible(CommandContext context, String visibleStr) {
+        if (!pluginStatus) return;
+        if (UserJudgeUtils.isConsoleCalling(context)) {
+            context.getSender().sendMessage("this command cannot be invoked from console! ");
+            return;
+        }
+        User user = context.getSender().getUser();
+        Contact contact = context.getSender().getSubject();
+        if (user == null || contact == null) {
+            context.getSender().sendMessage("用户为空！");
+            return;
+        }
+        long userID = user.getId();
+        if (userID == 0) {
+            LOGGER.warning("This command cannot be invoked in terminal! ");
+            return;
+        }
+        OsuSigBaseData oldData = OsuSigUtils.getBaseData(userID);
+        if (oldData == null) {
+            context.getSender().sendMessage("您尚未绑定用户！");
+            return;
+        }
+        OsuSigSettings settings = oldData.getOsuSigSettings();
+        switch (visibleStr.toLowerCase()) {
+            default:
+                context.getSender().sendMessage("无法解析这个参数");
+                return;
+            case "是":
+            case "true":
+            case "显示":
+                settings.setShowCountryRank(true);
+                break;
+            case "否":
+            case "false":
+            case "不显示":
+                settings.setShowCountryRank(false);
+                break;
+        }
+        OsuSigBaseData newData = new OsuSigBaseData(settings);
+        OsuSigUtils.setBaseData(userID, newData);
+        context.getSender().sendMessage("已设置: " + visibleStr);
+    }
+    @SubCommand({"setReplacePPWithRankScore", "srPPwrs", "替换PP为Rank分", "替换"})
+    @Description("设置是否替换PP为Rank分")
+    public void setReplacePPWithRankScore(CommandContext context, String replaceStr) {
+
+        if (!pluginStatus) return;
+        if (UserJudgeUtils.isConsoleCalling(context)) {
+            context.getSender().sendMessage("this command cannot be invoked from console! ");
+            return;
+        }
+        User user = context.getSender().getUser();
+        Contact contact = context.getSender().getSubject();
+        if (user == null || contact == null) {
+            context.getSender().sendMessage("用户为空！");
+            return;
+        }
+        long userID = user.getId();
+        if (userID == 0) {
+            LOGGER.warning("This command cannot be invoked in terminal! ");
+            return;
+        }
+        OsuSigBaseData oldData = OsuSigUtils.getBaseData(userID);
+        if (oldData == null) {
+            context.getSender().sendMessage("您尚未绑定用户！");
+            return;
+        }
+        OsuSigSettings settings = oldData.getOsuSigSettings();
+        switch (replaceStr.toLowerCase()) {
+            default:
+                context.getSender().sendMessage("无法解析这个参数");
+                return;
+            case "是":
+            case "true":
+            case "显示":
+                settings.setReplacePPWithRankScore(true);
+                break;
+            case "否":
+            case "false":
+            case "不显示":
+                settings.setReplacePPWithRankScore(false);
+                break;
+        }
+        OsuSigBaseData newData = new OsuSigBaseData(settings);
+        OsuSigUtils.setBaseData(userID, newData);
+        context.getSender().sendMessage("已设置: " + replaceStr);
+    }
+    @SubCommand({"setPPShowMode","sPPsm", "PP", "PP显示模式"})
+    @Description("设置PP显示模式")
+    public void setPPShowMode(CommandContext context, String ppShowModeStr) {
+        if (!pluginStatus) return;
+        if (UserJudgeUtils.isConsoleCalling(context)) {
+            context.getSender().sendMessage("this command cannot be invoked from console! ");
+            return;
+        }
+        User user = context.getSender().getUser();
+        Contact contact = context.getSender().getSubject();
+        if (user == null || contact == null) {
+            context.getSender().sendMessage("用户为空！");
+            return;
+        }
+        long userID = user.getId();
+        if (userID == 0) {
+            LOGGER.warning("This command cannot be invoked in terminal! ");
+            return;
+        }
+        OsuSigBaseData oldData = OsuSigUtils.getBaseData(userID);
+        if (oldData == null) {
+            context.getSender().sendMessage("您尚未绑定用户！");
+            return;
+        }
+        OsuSigSettings settings = oldData.getOsuSigSettings();
+        switch (ppShowModeStr.toLowerCase()) {
+            default:
+                context.getSender().sendMessage("无法解析这个参数");
+                return;
+            case "-1":
+            case "不显示":
+            case "无":
+            case "no":
+            case "false":
+            case "nodisplay":
+            case "no display":
+                settings.setPpShowMode(OsuSigEnum.PPShowMode.NO_DISPLAY);
+                break;
+            case "0":
+            case "替换等级":
+            case "替换":
+            case "replacelevel":
+            case "replace level":
+            case "replace":
+            case "level":
+                settings.setPpShowMode(OsuSigEnum.PPShowMode.REPLACE_LEVEL);
+                break;
+            case "1":
+            case "acc之后":
+            case "acc":
+            case "afteracc":
+            case "after acc":
+                settings.setPpShowMode(OsuSigEnum.PPShowMode.AFTER_ACC);
+                break;
+            case "2":
+            case "rank之后":
+            case "rank":
+            case "afterrank":
+            case "after rank":
+                settings.setPpShowMode(OsuSigEnum.PPShowMode.AFTER_RANK);
+                break;
+        }
+        OsuSigBaseData newData = new OsuSigBaseData(settings);
+        OsuSigUtils.setBaseData(userID, newData);
+        context.getSender().sendMessage("已设置: " + ppShowModeStr);
+    }
+    @SubCommand({"setExpVisible", "sev", "exp", "经验", "经验条", "显示经验"})
+    @Description("设置是否显示经验条")
+    public void setExpVisible(CommandContext context, String visibleStr) {
+
+        if (!pluginStatus) return;
+        if (UserJudgeUtils.isConsoleCalling(context)) {
+            context.getSender().sendMessage("this command cannot be invoked from console! ");
+            return;
+        }
+        User user = context.getSender().getUser();
+        Contact contact = context.getSender().getSubject();
+        if (user == null || contact == null) {
+            context.getSender().sendMessage("用户为空！");
+            return;
+        }
+        long userID = user.getId();
+        if (userID == 0) {
+            LOGGER.warning("This command cannot be invoked in terminal! ");
+            return;
+        }
+        OsuSigBaseData oldData = OsuSigUtils.getBaseData(userID);
+        if (oldData == null) {
+            context.getSender().sendMessage("您尚未绑定用户！");
+            return;
+        }
+        OsuSigSettings settings = oldData.getOsuSigSettings();
+        switch (visibleStr.toLowerCase()) {
+            default:
+                context.getSender().sendMessage("无法解析这个参数");
+                return;
+            case "是":
+            case "true":
+            case "显示":
+                settings.setShowExpBar(true);
+                break;
+            case "否":
+            case "false":
+            case "不显示":
+                settings.setShowExpBar(false);
+                break;
+        }
+        OsuSigBaseData newData = new OsuSigBaseData(settings);
+        OsuSigUtils.setBaseData(userID, newData);
+        context.getSender().sendMessage("已设置: " + visibleStr);
+    }
+
+    @SubCommand({"setSameBackgroundColor", "ssbc", "经验条背景同色", "同色", "经验条同色"})
+    @Description("设置经验条是否与背景同色")
+    public void setSameBackgroundColor(CommandContext context, String visibleStr) {
+        if (!pluginStatus) return;
+        if (UserJudgeUtils.isConsoleCalling(context)) {
+            context.getSender().sendMessage("this command cannot be invoked from console! ");
+            return;
+        }
+        User user = context.getSender().getUser();
+        Contact contact = context.getSender().getSubject();
+        if (user == null || contact == null) {
+            context.getSender().sendMessage("用户为空！");
+            return;
+        }
+        long userID = user.getId();
+        if (userID == 0) {
+            LOGGER.warning("This command cannot be invoked in terminal! ");
+            return;
+        }
+        OsuSigBaseData oldData = OsuSigUtils.getBaseData(userID);
+        if (oldData == null) {
+            context.getSender().sendMessage("您尚未绑定用户！");
+            return;
+        }
+        OsuSigSettings settings = oldData.getOsuSigSettings();
+        switch (visibleStr.toLowerCase()) {
+            default:
+                context.getSender().sendMessage("无法解析这个参数");
+                return;
+            case "是":
+            case "true":
+            case "显示":
+                settings.setExpBarUseSameColor(true);
+                break;
+            case "否":
+            case "false":
+            case "不显示":
+                settings.setExpBarUseSameColor(false);
+                break;
+        }
+        OsuSigBaseData newData = new OsuSigBaseData(settings);
+        OsuSigUtils.setBaseData(userID, newData);
+        context.getSender().sendMessage("已设置: " + visibleStr);
     }
     @SubCommand({"get","pic"})
     @Description("获取osu!sig名片")
@@ -172,7 +483,7 @@ public class OsuSig extends ArisuBotAbstractCompositeCommand {
             LOGGER.warning("This command cannot be invoked in terminal! ");
             return;
         }
-        MessageChainBuilder mcb = new MessageChainBuilder();
+        MessageChainBuilder mcb;
         String username = OsuUtils.getName(userID);
         if (username == null) {
             context.getSender().sendMessage("您尚未绑定osu!用户名");
@@ -193,12 +504,16 @@ public class OsuSig extends ArisuBotAbstractCompositeCommand {
             return;
         }
         ExternalResource resource = ExternalResource.create(Objects.requireNonNull(pictContent));
-        Image image = contact.uploadImage(resource);
-        mcb.add("您的用户名：" + username);
-        mcb.add(image);
-        contact.sendMessage(mcb.build());
+        try {
+            Image image = contact.uploadImage(resource);
+            mcb = new MessageChainBuilder();
+            mcb.add("用户" + username + "：");
+            mcb.add(image);
+            contact.sendMessage(mcb.build());
+        } catch (IllegalArgumentException e) {
+            context.getSender().sendMessage("获取osu!sig失败，可能是最近没有成绩或用户名有误");
+        }
     }
-    // TODO: 2025/1/16 Add settings for other 4 options.
 
     /**
      * Manages the plugin's operational state.
