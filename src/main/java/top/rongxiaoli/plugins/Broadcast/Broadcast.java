c45617ccda3114884ecf3d594be01ea3fd7d7c69
容小狸 <hashnoob079@163.com>
2025-01-18 00:02:31 +0800

    Apply code review suggestions

--- src/main/java/top/rongxiaoli/plugins/Broadcast/Broadcast.java
+++ src/main/java/top/rongxiaoli/plugins/Broadcast/Broadcast.java
@@ -99,8 +99,8 @@ public class Broadcast extends ArisuBotAbstractCompositeCommand {
         }
         executor.schedule(() -> {
             context.getSender().sendMessage("广播结束。");
+            isBroadcasting = false;
         }, delay + 100, TimeUnit.MILLISECONDS);
-        isBroadcasting = false;
         context.getSender().sendMessage("广播共发送了" + friendsList.size() + "个好友，共" + groupList.size() + "个群。");
 
     }
