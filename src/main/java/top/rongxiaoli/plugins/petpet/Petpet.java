46103ebf4a5754ca954e1c440947ee9b433e724b
容小狸 <58361774+rong-xiaoli@users.noreply.github.com>
2025-02-25 14:01:09 +0800

    [Feat] Petpet (#108)
    * Complete petpet.
    
    * Update src/main/java/top/rongxiaoli/plugins/petpet/Petpet.java
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: 容小狸 <58361774+rong-xiaoli@users.noreply.github.com>
    
    * Apply code review suggestions.
    
    * Finalize LOGGER in Petpet.
    
    ---------
    
    Signed-off-by: 容小狸 <58361774+rong-xiaoli@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ src/main/java/top/rongxiaoli/plugins/petpet/Petpet.java
@@ -0,0 +1,100 @@
+package top.rongxiaoli.plugins.petpet;
+
+import net.mamoe.mirai.console.command.CommandContext;
+import net.mamoe.mirai.message.data.*;
+import net.mamoe.mirai.utils.MiraiLogger;
+import org.jetbrains.annotations.NotNull;
+import top.rongxiaoli.backend.Commands.ArisuBotAbstractRawCommand;
+import top.rongxiaoli.backend.interfaces.Plugin;
+import top.rongxiaoli.plugins.petpet.backend.TargetResolver;
+
+import java.io.IOException;
+
+@Plugin(name = "Petpet")
+public class Petpet extends ArisuBotAbstractRawCommand {
+    public static final Petpet INSTANCE = new Petpet();
+    private volatile boolean pluginStatus = false;
+    private static final MiraiLogger LOGGER = MiraiLogger.Factory.INSTANCE.create(Petpet.class, "ArisuBot.Petpet");
+
+    public Petpet() {
+        super("pet", "摸", "petpet");
+        setDescription("摸一摸某人，不带参数摸自己，可以@某人，也可以输入QQ号摸对应用户");
+        setUsage("/pet {[@someone]|[QQID]}");
+        setPrefixOptional(true);
+    }
+
+    @Override
+    public void onCommand(@NotNull CommandContext context, @NotNull MessageChain args) {
+        if (args.isEmpty()) {
+            try {
+                TargetResolver.handle(context, String.valueOf(context.getSender().getUser().getId()));
+                return;
+            } catch (IOException e) {
+                MessageChainBuilder builder = new MessageChainBuilder();
+                builder.add(new QuoteReply(context.getOriginalMessage()));
+                builder.add("发生错误");
+                context.getSender().sendMessage(builder.build());
+                LOGGER.error(e);
+                return;
+            }
+        }
+        SingleMessage arg = args.get(0);
+        if (arg instanceof At) {
+            try {
+                TargetResolver.handleAt(context, (At) arg);
+            } catch (IOException e) {
+                MessageChainBuilder builder = new MessageChainBuilder();
+                builder.add(new QuoteReply(context.getOriginalMessage()));
+                builder.add("发生错误");
+                context.getSender().sendMessage(builder.build());
+                LOGGER.error(e);
+                return;
+            }
+            return;
+        }
+        try {
+            TargetResolver.handleString(context, arg.contentToString());
+        } catch (IOException e) {
+            MessageChainBuilder builder = new MessageChainBuilder();
+            builder.add(new QuoteReply(context.getOriginalMessage()));
+            builder.add("发生错误");
+            context.getSender().sendMessage(builder.build());
+            LOGGER.error(e);
+        }
+    }
+    @Override
+    public void load() {
+        LOGGER.debug("Petpet loading. ");
+        enablePlugin();
+        LOGGER.debug("Petpet loaded. ");
+    }
+
+    @Override
+    public void reload() {
+        LOGGER.debug("Petpet reloading. ");
+        enablePlugin();
+        LOGGER.debug("Petpet reloaded. ");
+    }
+
+    @Override
+    public void shutdown() {
+        LOGGER.debug("Petpet shutting down. ");
+        disablePlugin();
+        LOGGER.debug("Petpet shut down. ");
+    }
+
+    @Override
+    public synchronized void disablePlugin() throws IllegalStateException {
+        pluginStatus = false;
+    }
+
+    @Override
+    public synchronized void enablePlugin() throws IllegalStateException {
+        pluginStatus = true;
+    }
+
+    @Override
+    public boolean pluginStatus() {
+        return pluginStatus;
+    }
+}
---
+++ src/main/java/top/rongxiaoli/plugins/petpet/backend/TargetResolver.java
@@ -0,0 +1,50 @@
+package top.rongxiaoli.plugins.petpet.backend;
+
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.http.HttpUtil;
+import net.mamoe.mirai.console.command.CommandContext;
+import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.message.data.Image;
+import net.mamoe.mirai.utils.ExternalResource;
+import org.jetbrains.annotations.NotNull;
+import top.rongxiaoli.ArisuBot;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class TargetResolver {
+
+    private static final String API = "https://api.andeer.top/API/gif_mo.php?qq=";
+    public static void handleAt(@NotNull CommandContext context, At target) throws IOException{
+        String targetID = String.valueOf(target.getTarget());
+        handle(context, targetID);
+    }
+    public static void handleString(@NotNull CommandContext context, String target) throws IOException {
+        try {
+            long targetID = Long.parseLong(ReUtil.get("\\d+", target, 0));
+            handle(context, String.valueOf(targetID));
+        } catch (NumberFormatException e) {
+            context.getSender().sendMessage("请输入ID或直接@摸头对象");
+        }
+    }
+    public static void handle(@NotNull CommandContext context, String target) throws IOException {
+        File file = null;
+        File path = new File(ArisuBot.GetDataPath().toFile(), "petpet");
+        path.mkdirs();
+        file = File.createTempFile("Cache", ".gif", path);
+        byte[] content = HttpUtil.downloadBytes(API + target);
+        BufferedOutputStream stream = null;
+        stream = new BufferedOutputStream(Files.newOutputStream(file.toPath()));
+        stream.write(content);
+        stream.flush();
+        if (context.getSender().getSubject() == null) {
+            return;
+        }
+        Image image = ExternalResource.uploadAsImage(file, context.getSender().getSubject());
+        context.getSender().sendMessage(image);
+        file.delete();
+        stream.close();
+    }
+}
